1658d4d26e1547ecced5f69e2a0bf44b275f4da3
Hasan <akhmadkhasan68@gmail.com>
2021-03-03 09:01:39 +0700

    fix admin info function and add cronjob

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-var T = require('./twit');
+var config = require('./config');
 var express = require('express');
 var app = express();
-const _port = 3000;
+const _port = 8000;
 
-app.get('/', (req, res, next) => {
-    T.post('statuses/update', {
-        status: 'Halo!'
-    }, function(err, data, response)
-    {
-        res.json(data);
-    });
+const { TwitterBot } = require('./twitter-bot');
+const twitterbot = new TwitterBot({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token: config.access_token,
+    access_token_secret: config.access_token_secret,
 });
 
-app.listen(_port, () => { console.log(`server running on ${_port}`)});
+// app.get('/adminProfile', async (req, res, next) => {
+//     const admin = await twitterbot.getAdminUserInfo();
+//     res.json(admin);
+// });
+
+app.get('/adminProfile', async (req, res, next) => {
+    const admin = await twitterbot.getAdminUserInfo();
+    res.json(admin);
+});
+
+app.listen(_port, () => { console.log(`runnig on port ${_port}`) });
